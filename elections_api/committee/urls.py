6861c811e470c117dc16c50bd95e8e3e1c29f25a
grantholly <gdholly@gmail.com>
2018-05-06 16:15:01 -0700

    adding a route for total by committee id

--- elections_api/committee/urls.py
+++ elections_api/committee/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r"contributions/total/", views.total_contributions, name="version"),
     url(r"all_names", views.all_names, name="version"),
+    url(r"contributions/donors/(\d+)/total/", views.contribution_count, name="contribution_count"),
 ]
--- elections_api/committee/views.py
+++ elections_api/committee/views.py
@@ -43,3 +43,27 @@ def total_contributions(request):
                 "msg": res_clean,
             })
         # catch case where we don't have a result with a HTTP 404
+
+def contribution_count(request, cid): 
+    with psycopg2.connect(host="127.0.0.1",
+                          user="postgres",
+                          password="postgres",
+                          dbname="local_elections") as conn:
+        c = conn.cursor()
+        c.execute(
+            """
+            select count(*) as number_of_donors
+            from transactions t join transaction_details td
+            on t.transaction_id = td.transaction_id
+            where td.transaction_type = 'Contribution'
+            and t.committee_id = %(committee_id)s;
+            """, {"committee_id": cid}
+        )
+        res = c.fetchall()
+        if res:
+            res_clean = res[0][0]
+            print(res_clean)
+            return JsonResponse({
+                "msg": res_clean,
+            })
+        # catch case where we don't have a result with a HTTP 404
