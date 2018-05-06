cd8f021781fde07aed85ce3882c78cc46970d498
Marty <marty.bode@discoverorg.com>
2018-05-06 16:56:44 -0700

    adding top_donors

--- elections_api/committee/views.py
+++ elections_api/committee/views.py
@@ -1,6 +1,7 @@
 import psycopg2
 from django.shortcuts import render
 from django.http import JsonResponse
+import pandas as pd
 
 def all_names(request):
     with psycopg2.connect(host="127.0.0.1",
@@ -89,6 +90,31 @@ def contribution_count(request, cid):
             return JsonResponse({
                 "msg": res_clean,
             })
+
+def top_donors(request, cid):
+    test = 16423
+    with psycopg2.connect(host="127.0.0.1",
+                          user="postgres",
+                          password="postgres",
+                          dbname="local_elections") as conn:
+        c = conn.cursor()
+        c.execute(
+            """
+            select t.contributor_payee, sum(t.amount) from transactions t
+            left join transaction_details td using(transaction_id)
+            where t.committee_id = %(id)s and td.transaction_type = 'Contribution'
+			group by contributor_payee
+			order by sum(t.amount) desc limit 5
+            """,{"id": cid}
+        )
+        res = c.fetchall()
+        if res:
+            res_clean = res[0][0]
+            print(res_clean)
+            return JsonResponse({
+                "msg": res_clean,
+            })
+            
 def spending_categories(request, cid):
     test = 16423
     with psycopg2.connect(host="127.0.0.1",
